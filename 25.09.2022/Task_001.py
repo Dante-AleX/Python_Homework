896a3a5e6d1107f4e137c21897f4fa2e28cb6319
Alex <alex.kojokari42@gmail.com>
2022-09-30 16:27:59 +0300

    Completed Task_001

---
+++ 25.09.2022/Task_001.py
@@ -0,0 +1,12 @@
+# Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+# и проверяет, является ли этот день выходным.
+
+x = int (input('Please insert a number that corresponds to a day of the week: '))
+
+if x==6 or x==7:
+    print('This is a weekend day')
+elif x>=1 and x<=5:
+    print('This is a work day')
+else:
+    print('Invalid number')
+    

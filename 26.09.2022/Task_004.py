6a70351dd7676051268a88fe520ff946ff53174b
Alex <alex.kojokari42@gmail.com>
2022-10-01 11:11:50 +0300

    Completed Task_004

--- 26.09.2022/Task_004.py
+++ 26.09.2022/Task_004.py
@@ -2,3 +2,18 @@
 # Найдите произведение элементов на указанных позициях. 
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
+num = []
+number = []
+for i in range(1):
+    for j in range(-N, N + 1):
+        num.append(j)
+f = open('file.txt')
+n1 = int(f.read(1))
+n2 = int(f.read(2))
+mult = num[n1] * num[n2]
+print(f'Массив -> {num}. Произведение элементов на позициях {n1} и {n2} -> {mult}')
+for i in range(len(num)):
+    number.append(i)
+f.close()
+
+print(f'Перемешанный массив -> {num}')

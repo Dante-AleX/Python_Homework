2207d54cf0904db09821b41698a7e3ff521ca4a9
Alex <alex.kojokari42@gmail.com>
2022-10-01 11:34:47 +0300

    Completed Task_005

--- 26.09.2022/Task_005.py
+++ 26.09.2022/Task_005.py
@@ -1,1 +1,11 @@
 # Реализуйте алгоритм перемешивания списка.
+
+from random import randint
+
+list = [1, 2, 3, 4, 5, 6]
+for index_i in range(len(list)):
+    index_j = randint(0, len(list) - 1)
+    help = list[index_i]
+    list[index_i] = list[index_j]
+    list[index_j] = help
+print(list)

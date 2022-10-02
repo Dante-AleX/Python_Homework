59cd01c7f9be21c65cf32043f199db3420300e60
Alex <alex.kojokari42@gmail.com>
2022-10-02 10:16:11 +0300

    Completed Task_001

--- 26.09.2022/Task_001.py
+++ 26.09.2022/Task_001.py
@@ -1,17 +1,10 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 
-def Sum(n):
-
-    sum = 0
-    while (n != 0):
-
-        sum = sum + (n % 10)
-        n = n//10
-
-    return sum
-
-
-n = int(input('Please insert a real number: '))
-if n==0:
-    print('Invalid number')
-print(Sum(n))
+n = float(input('Введите вещественное число: '))
+sum = 0 
+while not n.is_integer(): 
+        n = n*10 
+while n != 0: 
+    sum += n % 10 
+    n //= 10
+print(sum)
